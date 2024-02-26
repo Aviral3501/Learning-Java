1833999b7640efb075a1ca1ad9fc2baf81e09bec
Aviral Singh <aviralsingh3501@gmail.com>
2024-02-26 17:23:06 +0530

    how are arrays stored in memory

--- src/Arrays/intro.java
+++ src/Arrays/intro.java
@@ -20,5 +20,37 @@ public class intro {
 
         //or print the array by -->
         System.out.println(Arrays.toString(roll_no));
+
+        // int[] roll_no --> declaration of the variable
+        //new int[5]; --> creating the object int the heap memory i.e. 5*int(bytes) of  space is allocated(may or may not be continuous)
+//            |
+//            V
+        //dynamic memory allocation happens at runtime ie during execution
+
+//        why array in java may not be continuous?
+        /**
+         * IN java -->
+         * 1)Objects are created in heap
+         * 2)Heap may or may not be contiguous
+         * Hence array in java may or may not be contiguous
+         * Java completely depends on JVM for the allocation of memory space
+         */
+
+//        In Java, arrays are contiguous in the sense that their elements are stored in consecutive memory locations. However, the misunderstanding
+//        may arise due to the fact that arrays in Java are objects, and when it comes to objects, the elements themselves are not stored directly
+//        in the array, but rather, references to objects are stored.
+
+
+//        So, while the array itself is a contiguous block of memory, the elements within the array (especially for objects) might be scattered in
+//        different parts of the memory, depending on where the objects are dynamically allocated. This contrasts with languages like C or C++, where
+//        arrays of objects store the actual objects in contiguous memory locations. Java's approach allows for better memory management and flexibility
+//        but introduces a layer of indirection through object references.
+
+// for  more detail --> https://chat.openai.com/share/59dd8468-581d-4ec6-9178-38d1ae50495b
+
+
+
+
+
     }
 }
